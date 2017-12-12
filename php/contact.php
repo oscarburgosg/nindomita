fb6fb659d8c2c84f73e7a95742762ff841e8a885
munozj <jcarom@deloitte.com>
2017-12-12 01:15:26 -0300

    #jcaro - english version

--- php/contact.php
+++ php/contact.php
@@ -4,17 +4,22 @@
 
 	// Check if name has been entered
 	if (!isset($_POST['name'])) {
-		$errors['name'] = 'Ingresa tu nombre';
+		$errors['name'] = 'Ingresa tu nombre/Enter your name';
+	}
+	
+  // Check if phone has been entered
+	if (!isset($_POST['phone'])) {
+		$errors['phone'] = 'Ingresa tu telefono/Enter your phone number';
 	}
 
 	// Check if email has been entered and is valid
 	if (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-		$errors['email'] = 'Ingresa tu email';
+		$errors['email'] = 'Ingresa tu email/Enter your email address';
 	}
 
 	//Check if message has been entered
 	if (!isset($_POST['message'])) {
-		$errors['message'] = 'Cuentanos tu inquietud';
+		$errors['message'] = 'Cuentanos tu inquietud/Enter your message';
 	}
 
 	$errorOutput = '';
@@ -40,13 +45,14 @@
 
 
 	$name = $_POST['name'];
+	$phone = $_POST['phone'];
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$from = $email;
 	$to = 'info@naturalezaindomita.cl';  // please change this email id
 	$subject = 'Contacto desde www.naturalezaindomita.cl';
 
-	$body = "Hola. Mi nombre es $name. Los contacto desde la web.\n Mi email es $email\n Les quiero preguntar lo siguiente:\n $message";
+	$body = "Hola. Mi nombre es $name - phone number $phone. Los contacto desde la web.\n Mi email es $email\n Les quiero preguntar lo siguiente:\n $message";
 
 	$headers = "From: ".$from;
 
@@ -56,7 +62,7 @@
 	if (mail ($to, $subject, $body, $headers)) {
 		$result .= '<div class="alert alert-success alert-dismissible" role="alert">';
  		$result .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-		$result .= '¡Gracias! estaremos en contacto';
+		$result .= '¡Gracias! estaremos en contacto / Thank you!, we will contact you ASAP';
 		$result .= '</div>';
 
 		echo $result;
@@ -66,7 +72,7 @@
 	$result = '';
 	$result .= '<div class="alert alert-danger alert-dismissible" role="alert">';
 	$result .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-	$result .= 'UPS! Por favor intenta nuevamente.';
+	$result .= 'UPS! Por favor intenta nuevamente. / Please try again';
 	$result .= '</div>';
 
 	echo $result;
